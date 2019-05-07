5bcf35bfe75572fb219b6c1cead0ac620b7aefa9
charlesuyso <darwin_uyso@yahoo.com>
2019-05-07 20:44:32 +0800

    Updates

---
+++ precache-manifest.a1f8d314a78e72b9de52e27924041ae5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e98764c0f2f3d908d50a572484e9d4",
+    "url": "/charlesuyso.github.io/index.html"
+  },
+  {
+    "revision": "387f56f0dd1205168f36",
+    "url": "/charlesuyso.github.io/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "b3d6daefab68bf7d45ca",
+    "url": "/charlesuyso.github.io/static/css/main.7d077930.chunk.css"
+  },
+  {
+    "revision": "387f56f0dd1205168f36",
+    "url": "/charlesuyso.github.io/static/js/2.419afc8d.chunk.js"
+  },
+  {
+    "revision": "b3d6daefab68bf7d45ca",
+    "url": "/charlesuyso.github.io/static/js/main.ac567479.chunk.js"
+  },
+  {
+    "revision": "eacfcfc5b2a65763a65c",
+    "url": "/charlesuyso.github.io/static/js/runtime~main.02f9a5a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/charlesuyso.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90e0eb3242c2c8880f7d928ff99ba85d",
+    "url": "/charlesuyso.github.io/static/media/polyimg.90e0eb32.svg"
+  },
+  {
+    "revision": "8548037bd586a43acab45a89220232a7",
+    "url": "/charlesuyso.github.io/static/media/raleway-bold.8548037b.woff"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "/charlesuyso.github.io/static/media/raleway-v13-latin-regular.43c849ea.woff2"
+  },
+  {
+    "revision": "5d060e06972a8e15b11b8a44991b07bb",
+    "url": "/charlesuyso.github.io/static/media/raleway-v13-latin-regular.5d060e06.ttf"
+  },
+  {
+    "revision": "60b344eb8dd676754364fc5ae4500d62",
+    "url": "/charlesuyso.github.io/static/media/raleway-v13-latin-regular.60b344eb.woff"
+  },
+  {
+    "revision": "e18be132ed71498dc498dcc99fe144b2",
+    "url": "/charlesuyso.github.io/static/media/raleway-v13-latin-regular.e18be132.eot"
+  },
+  {
+    "revision": "f6c57152f6515e2407ec51ee0b89e6f9",
+    "url": "/charlesuyso.github.io/static/media/raleway-v13-latin-regular.f6c57152.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cdu/precache-manifest.64b325d62fb69f2eaf8c077599d404e7.js"
+  "/charlesuyso.github.io/precache-manifest.a1f8d314a78e72b9de52e27924041ae5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cdu/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/charlesuyso.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
