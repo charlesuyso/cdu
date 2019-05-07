587933064513dad80cd820416189eaaaff101817
charlesuyso <darwin_uyso@yahoo.com>
2019-05-07 20:52:23 +0800

    add docs

---
+++ docs/precache-manifest.37a4104e234859693ce7c69882325898.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce9d282047aa9e8cf5ca838d7ef11ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13757ff7af4816e95b7",
+    "url": "/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "564ce753a3a0f77d9fd8",
+    "url": "/static/css/main.1ca9fae7.chunk.css"
+  },
+  {
+    "revision": "b13757ff7af4816e95b7",
+    "url": "/static/js/2.814f4704.chunk.js"
+  },
+  {
+    "revision": "564ce753a3a0f77d9fd8",
+    "url": "/static/js/main.5ebf24d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90e0eb3242c2c8880f7d928ff99ba85d",
+    "url": "/static/media/polyimg.90e0eb32.svg"
+  },
+  {
+    "revision": "8548037bd586a43acab45a89220232a7",
+    "url": "/static/media/raleway-bold.8548037b.woff"
+  },
+  {
+    "revision": "43c849ea0258ce0d23a480e840881f16",
+    "url": "/static/media/raleway-v13-latin-regular.43c849ea.woff2"
+  },
+  {
+    "revision": "5d060e06972a8e15b11b8a44991b07bb",
+    "url": "/static/media/raleway-v13-latin-regular.5d060e06.ttf"
+  },
+  {
+    "revision": "60b344eb8dd676754364fc5ae4500d62",
+    "url": "/static/media/raleway-v13-latin-regular.60b344eb.woff"
+  },
+  {
+    "revision": "e18be132ed71498dc498dcc99fe144b2",
+    "url": "/static/media/raleway-v13-latin-regular.e18be132.eot"
+  },
+  {
+    "revision": "f6c57152f6515e2407ec51ee0b89e6f9",
+    "url": "/static/media/raleway-v13-latin-regular.f6c57152.svg"
+  }
+]);
